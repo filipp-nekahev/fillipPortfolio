7f799fe24976f779c7cd7b435f5bfb98616e3159
Alexander Velichko <a.velichko@syde.com>
2025-10-15 22:04:52 +0300

    turn off image optimise

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,25 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import path from 'node:path';
+import path from "node:path";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-     resolve: {
+    resolve: {
       alias: {
         "@/*": path.resolve("./src/*"),
       },
+    },
+  },
+  compressHTML: true,
+  prefetch: {
+    prefetchAll: true,
+  },
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/noop",
     },
   },
 });
